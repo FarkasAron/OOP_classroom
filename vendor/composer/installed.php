182c0d3a17ecace6e419cc82498f8cfab856be6e
FarkasAron <147812207+FarkasAron@users.noreply.github.com>
2025-03-19 14:01:43 +0100

    Composer json add

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'ae02dfea4670be15e0fd4b96b7c62e63d5f39cdb',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'ae02dfea4670be15e0fd4b96b7c62e63d5f39cdb',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
